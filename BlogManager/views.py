7871b2143c7a6da5554a42b2670a32f61084aefd
feivs2019 <feivs2019@gmail.com>
2019-08-16 16:19:56 +0000

    refactorer BlogManager/views.py

--- BlogManager/views.py
+++ BlogManager/views.py
@@ -7,6 +7,69 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
 from django.urls import reverse_lazy
 
+class BlogBaseView(generic.ListView):
+    # 記事一覧画面をデフォルトとする
+    # それ以外の画面のビューとして継承する際は適宜書き換えること
+    model                   = TopicsTr
+    namespace               = 'list'
+    context_object_name     = 'model_data'
+    template_name           = 'BlogManager/list.html'
+    paginate_by             = 8
+    extract_length          = 40
+
+    def post(self, request, *args, **kwargs):
+        if(self.namespace == 'search'):
+            # 各ページからの検索ボタンによるPOST受付処理
+            search_value = [self.request.POST.get('search_words', None),]
+            request.session['search_words'] = search_value
+            # 検索時にページネーションに関連したエラーを防ぐ
+            self.request.GET = self.request.GET.copy()
+            self.request.GET.clear()
+            # GETリクエスト処理
+            return self.get(request, *args, **kwargs)
+
+    def get_queryset(self):
+        # 検索条件の設定
+        q_objects = Q(isdraft=False)
+        order_by = '-created_at'
+        if self.request.method == 'POST' and 'search_words' in self.request.session:
+            # 検索フォームからのPOSTリクエストの場合はキーワード検索
+            search_words = self.request.session['search_words']
+            order_by = '-likes'
+            for search_word in search_words:
+                q_objects |= Q(text__contains=search_word)
+        elif self.namespace == 'drafts':
+            # 下書き一覧の場合は下書きフラグが立っているもの
+            q_objects = Q(isdraft=True)
+        else:
+            pass
+        # 指定した検索条件によるモデルデータの取得とダイジェスト抽出
+        queryset = TopicsTr.objects.select_related().filter(q_objects).order_by(order_by)
+        for query in queryset:
+            query.text = extractDigest(query.text, self.extract_length)
+            query.created_at = query.created_at.date()
+        return queryset
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # ヘッダ情報の設定
+        context['page_title'] = "feivs2019's blog | " + self.namespace
+        context['keywords'] = "feivs2019,blog,Django,Python"
+        count = len(self.get_queryset())
+        # sessionに値がある場合、その値をセットする。（ページングしてもform値が変わらないように）
+        search_words = self.request.session['search_words'][0] if 'search_words' in self.request.session else ''
+        # Formオブジェクトの設定
+        context['search_form'] = SearchForm(initial={'search_words': search_words,})
+        context['upload_form'] = UploadForm()
+        context['post_form'] = PostForm()
+        # コンテンツ情報の設定
+        context['content_title'] = "記事一覧"
+        # ホーム画面の場合はページングしない
+        context['page_obj'] = None if self.namespace == 'home' else context['page_obj']
+        return context
+
+
 class UploadView(LoginRequiredMixin, generic.CreateView):
     """ファイルモデルのアップロードビュー POST専用"""
     model = MediaTr
@@ -15,22 +78,15 @@ class UploadView(LoginRequiredMixin, generic.CreateView):
     success_url = reverse_lazy('blog:post')
     
 
-class TopicView(generic.DetailView):
-    model = TopicsTr
-    context_object_name = 'model_data'
-    template_name = 'BlogManager/topic.html'
+class TopicView(BlogBaseView):
+    template_name           = 'BlogManager/topic.html'
+    namespace               = 'topic'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        topic_title = context['object'].title
-        context['page_title'] = "feivs2019's blog | " + topic_title
-        context['keywords'] = "feivs2019,blog,Django,Python"
-        # 検索フォームの取得
-        default_data = {
-                        'search_words': '', 
-                        }
-        search_form = SearchForm(initial=default_data)
-        context['search_form'] = search_form
+        # URLのpkの価で一意な情報を取得する
+        context['model_data'] = TopicsTr.objects.filter(id=self.kwargs['pk']).get()
+        context['page_title'] = "feivs2019's blog | " + context['model_data'].title
         return context
 
 
@@ -45,167 +101,39 @@ class PostView(LoginRequiredMixin, generic.CreateView):
         context = super().get_context_data(**kwargs)
         context['page_title'] = "feivs2019's blog | 記事を書く"
         context['keywords'] = "feivs2019,blog,Django,Python"
-        context['content_title'] = "記事を書く"
-        # 検索フォームの取得
-        default_data = {
-                        'search_words': '', 
-                        }
-        search_form = SearchForm(initial=default_data)
-        context['search_form'] = search_form
-        # アップロードフォームの取得
+        # Formオブジェクトの設定
+        search_words = self.request.session['search_words'][0] if 'search_words' in self.request.session else ''
+        context['search_form'] = SearchForm(initial={'search_words': search_words,})
         context['upload_form'] = UploadForm()
+        context['post_form'] = PostForm()
+        # コンテンツ情報の設定
+        context['content_title'] = "記事を書く"
         return context
 
 
-class DraftsView(LoginRequiredMixin, generic.ListView):
-# class DraftsView(generic.ListView):
-    model = TopicsTr
-    context_object_name = 'model_data'
-    template_name = 'BlogManager/list.html'
-    paginate_by = 10
-
-    def get_queryset(self):
-        queryset = TopicsTr.objects.select_related().filter(isdraft=True).order_by('-created_at')
-        # 記事ダイジェストの取得
-        for query in queryset:
-            query.text = extractDigest(query.text, 40)
-            query.created_at = query.created_at.date()
-        return queryset
+class DraftsView(LoginRequiredMixin, BlogBaseView):
+    namespace               = 'drafts'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['page_title'] = "feivs2019's blog | 下書き一覧"
-        context['keywords'] = "feivs2019,blog,Django,Python"
         context['content_title'] = "下書き一覧"
-
-        default_data = {
-                        'search_words': '', 
-                        }
-        search_form = SearchForm(initial=default_data)
-        context['search_form'] = search_form
-
         return context
 
 
-class ListView(generic.ListView):
-    model = TopicsTr
-    context_object_name = 'model_data'
-    template_name = 'BlogManager/list.html'
-    paginate_by = 10
-
-    def get_queryset(self):
-        queryset = TopicsTr.objects.select_related().filter(isdraft=False).order_by('-created_at')
-        # 記事ダイジェストの取得
-        for query in queryset:
-            query.text = extractDigest(query.text, 40)
-            query.created_at = query.created_at.date()
-        return queryset
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['page_title'] = "feivs2019's blog | 記事一覧"
-        context['keywords'] = "feivs2019,blog,Django,Python"
-        context['content_title'] = "記事一覧"
-
-        default_data = {
-                        'search_words': '', 
-                        }
-        search_form = SearchForm(initial=default_data)
-        context['search_form'] = search_form
-
-        return context
-
-
-class HomeView(generic.ListView):
-    model = TopicsTr
-    context_object_name = 'model_data'
+class HomeView(BlogBaseView):
     template_name = 'BlogManager/index.html'
-    get_count = 9
-    # paginate_by = 30
-
-    def get_queryset(self):
-        queryset = TopicsTr.objects.select_related().all().order_by('-created_at')[:self.get_count]
-        return queryset
+    namespace               = 'home'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['page_title'] = "feivs2019's blog"
-        context['keywords'] = "feivs2019,blog,Django,Python"
         context['content_title'] = "最新記事"
-
-        # 記事ダイジェストの取得
-        digests = []
-        for query in self.get_queryset():
-            digests.append({
-                'title'         : query.title
-                ,'text'          : extractDigest(query.text, 40)
-                ,'created_at'    : query.created_at.date()
-                ,'likes'         : query.likes
-            })
-        context['digests'] = digests
-
-        default_data = {
-                        'search_words': '', 
-                        }
-        search_form = SearchForm(initial=default_data)
-        context['search_form'] = search_form
-
         return context
 
 
 class SearchView(generic.ListView):
-    model = TopicsTr
-    context_object_name = 'model_data'
-    template_name = 'BlogManager/list.html'
-    get_count = 9
-    paginate_by = 10
-
-    def get_queryset(self):
-        # sessionに値がある場合、その値でクエリ発行する。
-        if 'search_words' in self.request.session:
-            search_words = self.request.session['search_words']
-            text = search_words[0]
-            # 検索条件
-            condition_text = Q()
-            if len(text) != 0 and text[0]:
-                condition_text = Q(text__contains=text)
-            # 記事ダイジェストの取得
-            queryset = TopicsTr.objects.select_related().filter(condition_text).filter(isdraft=False).order_by('-likes')
-            for query in queryset:
-                query.text = extractDigest(query.text, 40)
-                query.created_at = query.created_at.date()
-            return queryset
-        else:
-            # 何も返さない
-            return TopicsTr.objects.none()
-
-
-    def post(self, request, *args, **kwargs):
-        # 各ページからの検索ボタンによるPOST受付処理
-        search_value = [
-            self.request.POST.get('search_words', None),
-        ]
-        request.session['search_words'] = search_value
-        # 検索時にページネーションに関連したエラーを防ぐ
-        self.request.GET = self.request.GET.copy()
-        self.request.GET.clear()
-        # GETリクエスト処理
-        return self.get(request, *args, **kwargs)
-
+    namespace               = 'search'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['page_title'] = "feivs2019's blog | 検索結果"
-        context['keywords'] = "feivs2019,blog,Django,Python"
-        count = len(self.get_queryset())
-
-        # sessionに値がある場合、その値をセットする。（ページングしてもform値が変わらないように）
-        if 'search_words' in self.request.session:
-            search_words = self.request.session['search_words']
-            text = search_words[0]
-        default_data = {'search_words': text,}
-        search_form = SearchForm(initial=default_data)
-        context['search_form'] = search_form
-        context['content_title'] = text + "を含む記事一覧 (全" + str(count) + "件)"
-
+        context['content_title'] = text + "を含む記事一覧 (全" + str(len(context['object'])) + "件)"
         return context

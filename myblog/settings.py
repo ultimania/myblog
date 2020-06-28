3347e9d80d4ce2684310b792f05ee7ea7bef741d
takumi <yng.ultimania@gmail.com>
2020-06-28 18:39:01 +0900

    fix setting.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -1,11 +1,23 @@
 import os
-import dj_database_url
 
-# base settings
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '_!*7$ym_1wp4y&q5z&a(j4)7%0pr+hr$*g*v_)lzm#+vk_wj_l'
-ROOT_URLCONF = 'myblog.urls'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# ALLOWED_HOSTS = []
+
+
+# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -31,6 +43,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+ROOT_URLCONF = 'myblog.urls'
 
 TEMPLATES = [
     {
@@ -50,6 +63,28 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myblog.wsgi.application'
 
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+DATABASES = {
+   'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER_NAME', 'apiadmin'),
+        'PASSWORD': os.environ.get('DB_USER_PASS', 'apiadmin_pwd'),
+        'HOST': os.environ.get('DB_HOST', os.uname()[1]),
+        'PORT': os.environ.get('DB_PORT', '3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'sql_mode': 'traditional',
+        },
+        'TEST_NAME': 'auto_tests',
+   }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -67,38 +102,50 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
+# https://docs.djangoproject.com/en/2.1/topics/i18n/
+
 LANGUAGE_CODE = 'ja'
+
 TIME_ZONE = 'Asia/Tokyo'
+
 USE_I18N = True
+
 USE_L10N = True
+
 USE_TZ = True
 
+
 # Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
+# extra configuraion for my project
+ALLOWED_HOSTS = ['*']
+
+import os
+import pymysql
+import subprocess
+
+pymysql.install_as_MySQLdb()
+
+NOTEBOOK_ARGUMENTS = [
+    # exposes IP and port
+    '--ip=0.0.0.0',
+    '--port=8888',
+    # disables the browser
+    '--no-browser',
+]
+# extra configuraion for my project
+ALLOWED_HOSTS = ['*']
+
 # upload media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# redirect url
 LOGIN_REDIRECT_URL = '/blog/home'
 
-# extra configuraion for my project
-ALLOWED_HOSTS = ['*']
-
-# for heroku settings
-DATABASES = { 'default' : dj_database_url.config() }
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-DEBUG = False
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
-
-if DEBUG:
-    import django_heroku
-    django_heroku.settings(locals())
+DEBUG = True

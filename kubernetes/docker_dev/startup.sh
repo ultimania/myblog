f5dc9e697177aa3ed78cec11bfbcd592bd0c29a7
takumi <yng.ultimania@gmail.com>
2020-06-28 18:09:28 +0900

    fix

--- kubernetes/docker_dev/startup.sh
+++ kubernetes/docker_dev/startup.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 # python /opt/${PROJECT_NAME}/manage.py shell_plus --notebook
 git clone ${APP_REPOS} /opt/${PROJECT_NAME}
-cat ~/${PROJECT_NAME}/${PROJECT_NAME}/my_setting.py >> ~/opt/${PROJECT_NAME}/${PROJECT_NAME}/settings.py
+cat /root/my_setting.py >> ~/opt/${PROJECT_NAME}/${PROJECT_NAME}/settings.py
 python /opt/${PROJECT_NAME}/manage.py makemigrations
 python /opt/${PROJECT_NAME}/manage.py migrate
 python /opt/${PROJECT_NAME}/manage.py runserver 0.0.0.0:8080

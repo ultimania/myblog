547f16126eeba8f4e00c338f8cbf53f539697ca0
takumi <yng.ultimania@gmail.com>
2020-06-28 17:57:11 +0900

    change to

--- kubernetes/docker_dev/startup.sh
+++ kubernetes/docker_dev/startup.sh
@@ -1,7 +1,7 @@
 #/bin/sh
-# git clone -b twitter_api https://github.com/ultimania/investmentTools.git
 # python /opt/${PROJECT_NAME}/manage.py shell_plus --notebook
-cp -rp ~/${PROJECT_NAME} /opt
+git clone ${APP_REPOS} /opt/${PROJECT_NAME}
+cat ~/${PROJECT_NAME}/${PROJECT_NAME}/my_setting.py >> ~/opt/${PROJECT_NAME}/${PROJECT_NAME}/settings.py
 python /opt/${PROJECT_NAME}/manage.py makemigrations
 python /opt/${PROJECT_NAME}/manage.py migrate
 python /opt/${PROJECT_NAME}/manage.py runserver 0.0.0.0:8080

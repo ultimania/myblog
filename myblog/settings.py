1114d9f4bc620df33b0c41232ffa3fa0bcadd6ab
takumi <yng.ultimania@gmail.com>
2020-06-28 23:09:39 +0900

    f

--- myblog/settings.py
+++ myblog/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+#     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -60,7 +60,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myblog.wsgi.application'
+# WSGI_APPLICATION = 'myblog.wsgi.application'
 
 
 # Database
